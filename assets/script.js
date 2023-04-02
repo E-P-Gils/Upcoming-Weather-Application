466b5d42a28b0454349c373d1e71026accb813a6
unknown <sculptoid11@gmail.com>
2023-04-02 19:13:39 -0400

    Redo of this app, got the search function working. Finish local storage next.

--- assets/script.js
+++ assets/script.js
@@ -1,122 +1,38 @@
-var atlantaButton = document.getElementById("atlanta");
-var denverButton = document.getElementById("denver");
-var seattleButton = document.getElementById("seattle");
-var sanFranButton = document.getElementById("sanFran");
-var orlandoButton = document.getElementById("orlando");
-var nycButton = document.getElementById("nyc");
-var chicagoButton = document.getElementById("chicago");
-var austinButton = document.getElementById("austin");
-var currentWeather = document.getElementById("currentWeather");
-
-atlantaButton.addEventListener('click', function () {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=33.7488&lon=-84.3877&appid=b296dba86a3a90b522bbd546341c5512")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            var temp = data.list[0].main.temp;
-            var fahrenheit = Math.trunc((temp-273.15)*9/5+32); 
-            currentWeather.textContent = (fahrenheit + " degrees!");
-        })
-}
-)
-
-denverButton.addEventListener('click', function () {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=39.7392&lon=-104.9903&appid=b296dba86a3a90b522bbd546341c5512")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            var temp = data.list[0].main.temp;
-            var fahrenheit = Math.trunc((temp-273.15)*9/5+32); 
-            currentWeather.textContent = (fahrenheit + " degrees!");
-        })
-}
-)
-
-seattleButton.addEventListener('click', function () {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=47.6062&lon=-122.3321&appid=b296dba86a3a90b522bbd546341c5512")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            var temp = data.list[0].main.temp;
-            var fahrenheit = Math.trunc((temp-273.15)*9/5+32); 
-            currentWeather.textContent = (fahrenheit + " degrees!");
-        })
-}
-)
-
-sanFranButton.addEventListener('click', function () {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=37.7749&lon=-122.4194&appid=b296dba86a3a90b522bbd546341c5512")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            var temp = data.list[0].main.temp;
-            var fahrenheit = Math.trunc((temp-273.15)*9/5+32); 
-            currentWeather.textContent = (fahrenheit + " degrees!");
-        })
-}
-)
-
-orlandoButton.addEventListener('click', function () {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=28.5384&lon=-81.3789&appid=b296dba86a3a90b522bbd546341c5512")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            var temp = data.list[0].main.temp;
-            var fahrenheit = Math.trunc((temp-273.15)*9/5+32); 
-            currentWeather.textContent = (fahrenheit + " degrees!");
-        })
-}
-)
-
-nycButton.addEventListener('click', function () {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=40.7128&lon=-74.0060&appid=b296dba86a3a90b522bbd546341c5512")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            var temp = data.list[0].main.temp;
-            var fahrenheit = Math.trunc((temp-273.15)*9/5+32); 
-            currentWeather.textContent = (fahrenheit + " degrees!");
-        })
-}
-)
-
-chicagoButton.addEventListener('click', function () {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=41.8781&lon=-87.6298&appid=b296dba86a3a90b522bbd546341c5512")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            var temp = data.list[0].main.temp;
-            var fahrenheit = Math.trunc((temp-273.15)*9/5+32);
-            currentWeather.textContent = (fahrenheit + " degrees!");
-        })
-}
-)
-
-austinButton.addEventListener('click', function () {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=30.2672&lon=-97.7431&appid=b296dba86a3a90b522bbd546341c5512")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            var temp = data.list[0].main.temp;
-            var fahrenheit = Math.trunc((temp-273.15)*9/5+32); 
-            currentWeather.textContent = (fahrenheit + " degrees!");
-        })
-}
-)
-
+var submitButton = document.getElementById("submit");
+var weatherReport = document.getElementById('weather');
+var formStop = document.getElementById('form');
+submitButton.addEventListener('click', () => {
+    formStop.addEventListener('submit', (event) => {
+        event.preventDefault();
+        var citySearch = document.getElementById('city');
+        var city = citySearch.value;
+        var geoLocate = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b296dba86a3a90b522bbd546341c5512`;
+        var getCoords = function () {
+            fetch(geoLocate)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    var latitude = data.coord.lat;
+                    var longitude = data.coord.lon;
+                    var lat = `${latitude}`;
+                    var lon = `${longitude}`;
+                    var APIurl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=b296dba86a3a90b522bbd546341c5512`;
+                    fetch(APIurl)
+                        .then(function (response) {
+                            return response.json();
+                        })
+                        .then(function (data) {
+                            var temperature = data.list[0].main.temp;
+                            var temp = (temperature - 273.15) * 9/5 + 32;
+                            var city = citySearch.value;
+                            weatherReport.textContent = `The temperature in ${city} is ${temp} degrees!`
+                        })
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }
+        getCoords();
+    })
+});
